cdbb2d6d785a9f121b066345564aac7f2fe9ec73
dsantos747 <d.santos74747@gmail.com>
2023-09-11 12:02:40 +0100

    Updates

---
+++ precache-manifest.3ea7cdc61f7614cbe2621402b3ed9833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6fa131940539b6dac06",
+    "url": "/FCC-Quote-Machine/static/css/main.afaf1186.chunk.css"
+  },
+  {
+    "revision": "c6fa131940539b6dac06",
+    "url": "/FCC-Quote-Machine/static/js/main.c6fa1319.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/FCC-Quote-Machine/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/FCC-Quote-Machine/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "c9519eb01caf7912ecfa",
+    "url": "/FCC-Quote-Machine/static/js/runtime~main.c9519eb0.js"
+  },
+  {
+    "revision": "8ea33faa4ddf3d343780a9f57bba7867",
+    "url": "/FCC-Quote-Machine/index.html"
+  }
+];
